21aeddfd735a222f1ea8b0e3735d3cec1d58cc28
Emmil_Cheung <60263198+emmilcheung@users.noreply.github.com>
2020-04-08 19:24:33 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export const Header = () => {
+    function componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
     return (
         <h2>
             Expense Tracker
