7e98a337f5831553dcfee577d8657059a6eb7e24
emmilcheung <emmil_ubuntu@DESKTOP-C7BGQFF.localdomain>
2020-05-29 23:27:52 +0800

    animated loading

--- src/components/SchoolOptionList.js
+++ src/components/SchoolOptionList.js
@@ -1,32 +1,40 @@
-import React ,{ useState, useContext }from 'react';
+import React, { useState, useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import loadingGIF from '../data/loading.gif';
 
 export const SchoolOptionList = () => {
 
-    const { schoolList, loadBooklist} = useContext(GlobalContext);
+    const { schoolList, loadBooklist } = useContext(GlobalContext);
     //if fetching in global context not yet finished
-    if (schoolList == null || Object.keys(schoolList).length == 0){
-        return (<>Loading...</>)
+    if (schoolList == null || Object.keys(schoolList).length == 0) {
+        return (
+        <div className="loading">
+            <img src={loadingGIF} alt="" />
+            Loading...
+        </div>)
     }
     //
-    else{
+    else {
         // const [option, setOption] = useState('');
-        const onChange = e =>{
-            e.preventDefault();
-            loadBooklist({schoolId : e.target.value, year : 'form1'});
+        const onChange = e => {
+            // e.preventDefault();
+            if (e.target.value !== 'default') {
+                loadBooklist({ schoolId: e.target.value, year: 'form1' });
+            }
         }
-        return(
+        return (
             <>
                 <select name="school" id="school" onChange={onChange}>
-                {/* <option value="volvo">Volvo</option>
+                    {/* <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="opel">Opel</option>
                 <option value="audi">Audi</option> */}
-                {
-                    Object.keys(schoolList.bookList).map(id => (
-                        <option value={id}>{schoolList.bookList[id].schoolName}</option>
-                    ))
-                }
+                    <option value="default">------Please choose a school------</option>
+                    {
+                        Object.keys(schoolList.bookList).map(id => (
+                            <option value={id}>{schoolList.bookList[id].schoolName}</option>
+                        ))
+                    }
                 </select>
             </>
         )
